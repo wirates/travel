76e7ddd2d05b2213bedc0554ee4649da5d0f6db5
Your Name <wiratesltd@gmail.com>
2012-05-19 15:19:26 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travel::Application.config.secret_token = 'c1ba51c3c83e2af6f004484a726a991e2bd4f8458cf3d04366062bd5596a0e9e618842518133c8aa574035b9d92f9bfdef71f63570e97cf4de7a13f7ecd4ec3a'
